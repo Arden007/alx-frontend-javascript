ba90c1ccfbbcde1114be5a7ca67d4ad80800a6f3
Arden007 <cloveryuno664@gmail.com>
2023-09-29 20:55:24 +0200

    fix

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(func) {
-    let queue = [];
-    try {
-        const res = func();
-        queue.push(res);
-    } catch (error) {
-        queue.push(`${error}`);
-    }finally{
-        queue.push("Guardrail was processed");
-    }
-    return queue;    
+  const queue = [];
+  try {
+    const res = func();
+    queue.push(res);
+  } catch (error) {
+    queue.push(`${error}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
