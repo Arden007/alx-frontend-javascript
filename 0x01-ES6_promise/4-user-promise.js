88174002fa85636aad062d628f753530ea3a94ac
Arden007 <cloveryuno664@gmail.com>
2023-09-29 20:44:07 +0200

    fix error

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,12 +1,19 @@
 export default function signUpUser(firstName, lastName) {
-    return new Promise((resolve, reject) => {
-      if (firstName && lastName !== undefined| "") {
-        resolve({
-          firstName: firstName,
-          lastName: lastName,
-        });
-      } else {
-        reject(new Error("firstName, lastName undefined or empty string"));
-      }
-    });
+  return new Promise((resolve, reject) => {
+    if (
+      firstName !== undefined &&
+      lastName !== undefined &&
+      firstName !== '' &&
+      lastName !== ''
+    ) {
+      resolve({
+        firstName: firstName,
+        lastName: lastName,
+      });
+    } else {
+      reject(
+        new Error('firstName or lastName is undefined or an empty string')
+      );
+    }
+  });
 }
