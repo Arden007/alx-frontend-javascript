134e13bc7b99d1ced1098d166452239d75511b1c
Arden007 <cloveryuno664@gmail.com>
2023-10-01 03:35:11 +0200

    fix

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,8 @@
 export default function returnHowManyArguments(...args) {
-    let total = 0;
-    for (const arg of args) {
-        total += 1;
-    }
-    return total;
+  let total = 0;
+  let arg = ""
+  for (arg of args) {
+      total += 1;
+  }
+  return total;
 }
