5b6dfa2277ba0b62ff53747ae70f44849cad2bda
Arden007 <cloveryuno664@gmail.com>
2023-10-03 22:28:56 +0200

    fix

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
-export default function getStudentIdsSum(arr){
+export default function getStudentIdsSum(arr) {
   const sum = arr.reduce((previousValue, student) => {
     return previousValue + student.id;
   }, 0);
